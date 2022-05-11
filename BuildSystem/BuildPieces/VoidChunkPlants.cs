22b3986aed92f3e19b6b048ba202ba2c76d07f1b
Reika <reikasminecraft@gmail.com>
2022-05-11 12:20:02 -0400

    Update 29

--- BuildSystem/BuildPieces/VoidChunkPlants.cs
+++ BuildSystem/BuildPieces/VoidChunkPlants.cs
@@ -30,28 +30,62 @@ namespace ReikaKalseki.SeaToSea
 		
 		private List<GameObject> gennedMushrooms = new List<GameObject>();
 		
-		private static readonly Dictionary<VanillaFlora, float[]> depthRanges = new Dictionary<VanillaFlora, float[]>();
+		private static readonly Dictionary<VanillaFlora, VoidPlant> plantTypes = new Dictionary<VanillaFlora, VoidPlant>();
+		
+		static VoidChunkPlants() {
+			addPlantType(new VoidPlant(VanillaFlora.GABE_FEATHER, 100));
+			addPlantType(new VoidPlant(VanillaFlora.GHOSTWEED, 650, 5000, 700, 900, 50, 85));
+			addPlantType(new VoidPlant(VanillaFlora.MEMBRAIN, 0, 800, 20));
+			addPlantType(new VoidPlant(VanillaFlora.REGRESS, 0, 700, 500, 700, 30, 10));
+			addPlantType(new VoidPlant(VanillaFlora.BRINE_LILY, 50));
+			addPlantType(new VoidPlant(VanillaFlora.AMOEBOID, 750, 5000, 10));
+			addPlantType(new VoidPlant(VanillaFlora.BLOOD_KELP, 10));
+			addPlantType(new VoidPlant(VanillaFlora.VIOLET_BEAU, 0, 600, 500, 600, 20, 5));
+			/*
+			List<VoidPlant> li = new List<VoidPlant>(plantTypes.Values);
+			List<string> csv = new List<string>();
+			for (int y = -450; y >= -1100; y -= 10) {
+				SBUtil.log("@ Depth = "+(-y)+":");
+				double sum = 0;
+				string line = (-y)+"";
+				foreach (VoidPlant p in li) {
+					sum += p.getWeight(y);
+				}
+				foreach (VoidPlant p in li) {
+					double wt = p.getWeight(y);
+					double frac = wt/sum;
+					double pct = frac*100;
+					SBUtil.log(p.plant.getName()+" ["+p.minDepth+"-"+p.maxDepth+"]: wt="+wt+" > "+pct+"%");
+					line = line+","+pct;
+				}
+				csv.Add(line);
+			}
+			string header = "depth";
+			foreach (VoidPlant p in li) {
+				header = header+","+p.plant.getName();
+			}
+			csv.Insert(0, header);
+			SBUtil.log("CSV EXPORT");
+			SBUtil.log(string.Join("\n", csv));
+			SBUtil.log("========");*/
+		}
+		
+		private static void addPlantType(VoidPlant vp) {
+			plantTypes[vp.plant] = vp;
+		}
 		
 		public VoidChunkPlants(Vector3 vec) : base(vec) {
 			this.fuzz = new Vector3(1.2F, 0.05F, 1.2F);
 			this.count = UnityEngine.Random.Range(1, 4); //1-3
 			this.preferLit = true;
 			
-			addPlant(VanillaFlora.GABE_FEATHER, 100, 0, 5000);
-			addPlant(VanillaFlora.GHOSTWEED, 85, 650, 5000);
-			addPlant(VanillaFlora.MEMBRAIN, 20, 0, 800);
-			addPlant(VanillaFlora.REGRESS, 10, 0, 700);
-			addPlant(VanillaFlora.BRINE_LILY, 50, 0, 5000);
-			addPlant(VanillaFlora.AMOEBOID, 10, 750, 5000);
-			addPlant(VanillaFlora.BLOOD_KELP, 10, 0, 5000);
-			addPlant(VanillaFlora.VIOLET_BEAU, 20, 0, 600);
+			foreach (VoidPlant p in plantTypes.Values) {
+				double wt = p.getWeight(vec.y);
+				if (wt > 0)
+					plants.addEntry(p.plant, wt);
+			}
 			
 			mushrooms = UnityEngine.Random.Range(0, 7); //0-6
-		}
-		
-		private void addPlant(VanillaFlora vf, double wt, float minDepth, float maxDepth) {
-			plants.addEntry(vf, wt);
-			depthRanges[vf] = new float[]{minDepth, maxDepth};
 		}
 		
 		public override void generate(List<GameObject> li) {
@@ -68,6 +102,9 @@ namespace ReikaKalseki.SeaToSea
 		
 		private bool isPlantAllowed(VanillaFlora vf) {
 			if (vf == VanillaFlora.BLOOD_KELP && !allowKelp)
+				return false;
+			VoidPlant vp = plantTypes[vf];
+			if (position.y < vp.minDepth || position.y > vp.maxDepth)
 				return false;
 			return true;
 		}
@@ -103,6 +140,47 @@ namespace ReikaKalseki.SeaToSea
 			base.saveToXML(e);
 			
 			e.addProperty("mushrooms", mushrooms);
+		}
+		
+		private class VoidPlant {
+			
+			internal readonly VanillaFlora plant;
+			internal readonly float minDepth;
+			internal readonly float maxDepth;
+			internal readonly float minDepthWeightPoint;
+			internal readonly float maxDepthWeightPoint;
+			private readonly double weightAtMin;
+			private readonly double weightAtMax;
+			
+			internal VoidPlant(VanillaFlora vf, double wt) : this(vf, 0, 5000, wt) {
+				
+			}
+			
+			internal VoidPlant(VanillaFlora vf, float min, float max, double wt) : this(vf, min, max, min, max, wt, wt) {
+				
+			}
+			
+			internal VoidPlant(VanillaFlora vf, float min, float max, float minp, float maxp, double wtMin, double wtMax) {
+				plant = vf;
+				minDepth = min;
+				maxDepth = max;
+				minDepthWeightPoint = minp;
+				maxDepthWeightPoint = maxp;
+				weightAtMin = wtMin;
+				weightAtMax = wtMax;
+			}
+			
+			internal double getWeight(double y) {
+				double depth = -y;
+				if (depth < minDepth || depth > maxDepth)
+					return 0;
+				if (depth <= minDepthWeightPoint)
+					return weightAtMin;
+				if (depth >= maxDepthWeightPoint)
+					return weightAtMax;
+				return MathUtil.linterpolate(depth, minDepthWeightPoint, maxDepthWeightPoint, weightAtMin, weightAtMax);
+			}
+			
 		}
 		
 	}
